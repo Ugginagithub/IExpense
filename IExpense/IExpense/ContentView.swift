2e0437a1c67e642e58874276206a15da45e4e53b
Tarun Uggina <tarun.uggina@gmail.com>
2026-06-16 16:22:45 +0530

    Learnt about observable, bottomSheet
    Learnt about observable, bottomSheet, handled with second view

--- IExpense/IExpense/ContentView.swift
+++ IExpense/IExpense/ContentView.swift
@@ -6,16 +6,52 @@
 //
 
 import SwiftUI
+//struct User{
+//    var firstName = "Tarun"
+//    var lastName = "Kumar"
+//}
+
+//with class
+@Observable //the values will update only when this keyword is given, else the values will not update with classes
+class User{
+    var firstName = "Tarun"
+    var lastName = "Kumar"
+}
+
+//MARK: Showing and hiding views
+struct SecondView: View{
+    @Environment(\.dismiss) var dismiss
+    let name : String
+    
+    var body: some View{
+//        Text("Hello, \(name)")
+        Button("Dismiss"){
+            dismiss()
+        }
+    }
+}
 
 struct ContentView: View {
+    @State private var user = User() //if this variable chanegs the view will be reloaded.
+    
+    @State private var showingSheet = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+//        VStack {
+//            Text("Hello, \(user.firstName) \(user.lastName)")
+//            
+//            TextField("First Name", text: $user.firstName)
+//            TextField("Last Name", text: $user.lastName)
+//        }
+//        .padding()
+        
+        //MARK: we will get a bottomSheet here wow...
+        Button("Show sheet"){
+            showingSheet.toggle()
         }
-        .padding()
+        .sheet(isPresented: $showingSheet){
+            SecondView(name: "Tarun")
+        }
     }
 }
 
