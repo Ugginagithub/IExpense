1031e1a5b33ecc6b550f77bbb17b8f8f47afd32b
UGGINA TARUN PAVAN KUMAR <98165814+Ugginagithub@users.noreply.github.com>
2026-06-16 13:00:43 +0530

    Learn about data transfer between screen
    Learn about data transfer between screen

---
+++ IExpense/IExpense/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IExpense
+//
+//  Created by Tarun on 16/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IExpense/IExpense/IExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  IExpenseApp.swift
+//  IExpense
+//
+//  Created by Tarun on 16/06/26.
+//
+
+import SwiftUI
+
+@main
+struct IExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
